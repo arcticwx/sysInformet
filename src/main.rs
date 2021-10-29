8eaa8acaa1ed612301e6bc8f6f93fead21ad9bee
Nazxghul <nazxghul@gmail.com>
2021-10-29 23:40:08 +0100

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use std::env;
+
+//TODO: Make "Hello" Give Users Name.
+//TODO: Ask Users Name On First Launch.
+//TODO: Add File To Remember Variables Like Users Name.
+//TODO: Add More. 
+
+fn main() {
+    println!("\n");
+    println!("\n");
+    println!("\n");
+    println!("|------------------------|");
+    println!("|   Hello!               |");
+    println!("|   Os Type: {}     |", env::consts::OS);
+    println!("|                        |");
+    println!("|                        |");
+    println!("|                        |");
+    println!("|                        |");
+    println!("|------------------------|");
+    println!("\n");
+    println!("\n");
+    println!("\n");
+}
