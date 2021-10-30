b0295dfc8347aedde975a4f8a0ae36e8a523d39b
Nazxghul <nazxghul@gmail.com>
2021-10-30 10:23:32 +0100

    Added more system information using a crate called sysinfo

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,24 @@
 use std::env;
+use sysinfo::{NetworkExt, NetworksExt, ProcessExt, System, SystemExt};
 
-//TODO: Make "Hello" Give Users Name.
-//TODO: Ask Users Name On First Launch.
-//TODO: Add File To Remember Variables Like Users Name.
+//TODO: REFORMAT DESIGN
 //TODO: Add More. 
 
 fn main() {
+    let mut sys = System::new_all();
+    sys.refresh_all();
+
     println!("\n");
     println!("\n");
     println!("\n");
-    println!("|------------------------|");
-    println!("|   Hello!               |");
-    println!("|   Os Type: {}     |", env::consts::OS);
-    println!("|                        |");
-    println!("|                        |");
-    println!("|                        |");
-    println!("|                        |");
-    println!("|------------------------|");
+    println!("|------------------------------------------------|");
+    println!("|   Hello {:?}                |", sys.host_name());
+    println!("|   Os Type: {}                             |", env::consts::OS);
+    println!("|   Kernel Version: {:?}                |", sys.kernel_version());
+    println!("|   Total Memory: {} KB                     |", sys.total_memory());
+    println!("|   Processors: {}                               |", sys.processors().len());
+    println!("|   Total Swap: {}                          |", sys.total_swap());
+    println!("|------------------------------------------------|");
     println!("\n");
     println!("\n");
     println!("\n");
