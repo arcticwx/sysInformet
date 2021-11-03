a1bc4aef693e45347e43754aca66db8f3fbc3e0e
Nazxghul <nazxghul@gmail.com>
2021-11-03 20:02:44 +0000

    Reformatted to accomodate for larger info, moved data around to make more sense

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::io::prelude::*;
 use std::env;
 use sysinfo::{NetworkExt, NetworksExt, ProcessExt, System, SystemExt};
 
-//TODO: REFORMAT DESIGN
+//TODO: REFORMAT DESIGN -- Semi Done
 //TODO: Add More. 
 
 fn pause() {
@@ -25,14 +25,14 @@ fn main() {
     println!("\n");
     println!("\n");
     println!("\n");
-    println!("|------------------------------------------------|");
-    println!("|   Hello {:?}                |", sys.host_name());
-    println!("|   Os Type: {}                             |", env::consts::OS);
-    println!("|   Kernel Version: {:?}                |", sys.kernel_version());
-    println!("|   Total Memory: {} KB                     |", sys.total_memory());
-    println!("|   Processors: {}                               |", sys.processors().len());
-    println!("|   Total Swap: {}                          |", sys.total_swap());
-    println!("|------------------------------------------------|");
+    println!("------------------------------------------------");
+    println!("   Hello {:?}                                   ", sys.host_name());
+    println!("   Os Type: {}                                  ", env::consts::OS);
+    println!("   Kernel Version: {:?}                         ", sys.kernel_version());
+    println!("   Processors: {}                               ", sys.processors().len());
+    println!("   Total Memory: {} KB                          ", sys.total_memory());
+    println!("   Total Swap: {} KB                            ", sys.total_swap());
+    println!("------------------------------------------------");
     println!("\n");
     println!("\n");
     println!("\n");
